53fe4292fbe5447e09c692e97b98bdbe35eb93ef
yuns-emre <eskode145@gmail.com>
2021-01-23 18:10:45 +0300

    Dictonary

---
+++ Dictonary/MyDictonary.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictonary
+{
+    class MyDictonary <T,K>
+    {
+        // T  = Key K = value 
+
+        T[] key;
+        K[] value;
+        T[] tempKey;
+        K[] tempValue;
+        public MyDictonary()
+        {
+            key = new T[0];
+            value = new K[0];
+
+        }
+        public void Add(T name,K id )
+        {
+            tempKey = key;
+            tempValue = value;
+
+            key = new T[key.Length + 1];
+            value = new K[value.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                tempKey[i] = key[i];
+            }
+
+            key[key.Length - 1] = name;
+
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                tempValue[i] = value[i];
+            }
+
+            value[value.Length - 1] = id;
+
+
+        }
+
+        public int CountKey
+        {
+            get { return key.Length; }
+        }
+
+        public int CountValue
+        {
+            get { return value.Length; }
+        }
+
+
+    }
+
+}
---
+++ Dictonary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Dictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictonary<string,int> myDictonary = new MyDictonary<string,int>();
+            myDictonary.Add("Amy",8);
+            myDictonary.Add("Jake", 5);
+            myDictonary.Add("Terry", 10);
+
+            Console.WriteLine(myDictonary.CountKey);
+            Console.WriteLine(myDictonary.CountValue);
+        }
+    }
+}
